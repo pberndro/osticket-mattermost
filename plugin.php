7bf659fab28fe378e1c3d8ac4b62debbb2900c4a
Philip Berndroth <p.berndroth@philuweb.de>
2016-08-21 22:53:21 +0200

    create plugin.php

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'id' =>             'osticket:mattermost', 
+    'version' =>        '0.1',
+    'name' =>           'Mattermost integration',
+    'author' =>         'Philip Berndroth',
+    'description' =>    'Send message in Mattermost for new ticket and ticket changes.',
+    'url' =>            'https://github.com/pberndro/osticket-mattermost',
+    'plugin' =>         'mattermost.php:MattermostPlugin',
+);
+
+?>
